1f4d2c26e614911c4677c324fbbb1ccfbbcf90ec
quantinium3 <quant@quantinium.dev>
2026-07-10 19:57:19 +0530

    feat: add security group to vpn to allow egress

--- terraform/module/vpn/main.tf
+++ terraform/module/vpn/main.tf
@@ -1,8 +1,28 @@
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "${var.name}-client-vpn"
+  description = "Allow the Client VPN endpoint to reach resources in the VPC"
+  vpc_id      = var.vpc_id
+
+  egress_with_cidr_blocks = [
+    {
+      rule        = "all-all"
+      cidr_blocks = var.vpc_cidr_block
+    }
+  ]
+
+  tags = var.tags
+}
+
 resource "aws_ec2_client_vpn_endpoint" "this" {
   description            = "${var.name}-client-vpn"
   server_certificate_arn = var.server_certificate_arn
   client_cidr_block      = var.client_cidr_block
   vpc_id                 = var.vpc_id
+  split_tunnel           = true
+  security_group_ids     = [module.security_group.security_group_id]
 
   authentication_options {
     type                       = "certificate-authentication"
